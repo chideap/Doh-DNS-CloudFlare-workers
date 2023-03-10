1bae5375da142351695faabdd1de79e4b67662ad
chideap <40379524+chideap@users.noreply.github.com>
2023-03-10 17:37:29 +0800

    1736
    create

---
+++ do.js
@@ -0,0 +1,25 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  //查询路径'abc'，可以修改成自建想要的
+  if (url.pathname === '/abc') {
+    const dohUrl = 'https://1.1.1.1/dns-query'
+    const headers = new Headers(request.headers)
+    headers.set('Host', 'm.baidu.com')//可以不要
+    headers.set('Referer', 'no-referrer')//可以不要
+    headers.set('Content-Type', 'application/dns-message')
+    const dohRequest = new Request(dohUrl, {
+      method: request.method,
+      headers: headers,
+      body: request.body,
+      passThroughOnException: true
+    })
+    return fetch(dohRequest)
+  } else {
+    const response = await fetch(request)
+    return response
+  }
+}
